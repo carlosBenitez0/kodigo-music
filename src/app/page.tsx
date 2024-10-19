c6b090ce5816472f096b6a5c5ff7fafbdb654984
Carlos Benítez <cb2724136@gmail.com>
2024-10-19 09:54:02 -0600

    feat: add background bubbles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,30 @@ import { Main } from "./shared/components/main/Main";
 
 export default function Home() {
   return (
-    <div className="min-h-screen w-full grid grid-cols-[auto,1fr] grid-rows-[auto,1fr]">
-      <Aside />
-      <Header />
-      <Main />
+    <div
+      className="min-h-screen w-full bg-transparent  
+    relative z-0"
+    >
+      {/* <Bubble
+        left="100px"
+        top="100px"
+        blur="md"
+        firstColor="green"
+        secondColor="purple"
+        w="200px"
+        h="200px"
+        bgDireccion="br"
+      /> */}
+      <div className=" left-72 top-72 bg-gradient-to-br blur-sm from-purple-500 to-green-500 w-48 h-48 rounded-full absolute"></div>
+      <div className="left-20 top-60 bg-gradient-to-bl blur-lg from-orange-500 to-green-500 w-10 h-10 rounded-full absolute"></div>
+      <div className=" right-96 top-20 bg-gradient-to-tr blur-md from-yellow-500 to-red-500 w-20 h-20 rounded-full absolute"></div>
+      <div className=" right-10 top-20 bg-gradient-to-br blur-md from-blue-500 to-purple-500 w-96 h-96 rounded-full absolute"></div>
+
+      <div className="z-10 min-h-screen w-full grid grid-cols-[auto,1fr] grid-rows-[auto,1fr] relative">
+        <Aside />
+        <Header />
+        <Main />
+      </div>
     </div>
   );
 }
--- src/app/shared/components/main/Main.tsx
+++ src/app/shared/components/main/Main.tsx
@@ -2,7 +2,7 @@ import { CardSong } from "./Cards";
 
 export const Main = () => {
   return (
-    <main className="border-2 border-black p-8">
+    <main className=" p-8">
       <div className="flex items-center gap-8 ">
         <CardSong />
         <CardSong />
