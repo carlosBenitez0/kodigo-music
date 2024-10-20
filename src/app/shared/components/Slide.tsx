7636e3f7f6a5306c027db78d031959cd31f59c30
Carlos Benítez <cb2724136@gmail.com>
2024-10-20 09:31:53 -0600

    small changes

--- src/app/shared/components/Slide.tsx
+++ src/app/shared/components/Slide.tsx
@@ -3,8 +3,36 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/swiper-bundle.css";
 import { Keyboard, Mousewheel, Navigation } from "swiper/modules";
 import { CardSong } from "./main/Cards";
+import { useEffect, useState } from "react";
 
 export const Slide = () => {
+  const [songs, setSongs] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      const url =
+        "https://spotify23.p.rapidapi.com/search/?type=multi&offset=0&limit=10&numberOfTopResults=5";
+      const options = {
+        method: "GET",
+        headers: {
+          "x-rapidapi-key":
+            "94aa133bb8msh7ce1e94598f1f27p1f38ccjsnde4f08b81105",
+          "x-rapidapi-host": "spotify23.p.rapidapi.com",
+        },
+      };
+      console.log(options);
+
+      try {
+        const response = await fetch(url, options);
+        const result = await response.text();
+        console.log(result);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getData();
+  }, []);
+
   return (
     <Swiper
       breakpoints={{
